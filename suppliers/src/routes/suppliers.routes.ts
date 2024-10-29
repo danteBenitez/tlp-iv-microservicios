42c0e21ab1ab053cc36bf0315747ba1144772367
alexsh882 <alexsh882@gmail.com>
2024-10-29 09:30:25 -0300

    feat: Agregar middleware de autenticación a las rutas de proveedores

--- suppliers/src/routes/suppliers.routes.ts
+++ suppliers/src/routes/suppliers.routes.ts
@@ -1,22 +1,23 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { SupplierController } from "../controllers/suppliers.controller";
 import { roleMiddleware } from "../middleware/role.middleware";
 import { suppliersService } from "../services/supplier.service";
+import { authMiddleware } from "../middleware/auth.middleware";
 
 const controller = new SupplierController(suppliersService);
 
 const router: Router = Router();
 
-router.get('/', (req, res) => controller.findAllSuppliers(req, res));
+router.get('/',[authMiddleware], (req: Request, res: Response) => controller.findAllSuppliers(req, res));
 
-router.get('/:supplierId', (req, res) => controller.findById(req, res));
+router.get('/:supplierId',[authMiddleware], (req: Request, res: Response) => controller.findById(req, res));
 
 router.use(roleMiddleware("admin"));
 
-router.post('/', (req, res) => controller.create(req, res));
+router.post('/', (req: Request, res: Response) => controller.create(req, res));
 
-router.patch('/:supplierId', (req, res) => controller.update(req, res));
+router.patch('/:supplierId', (req: Request, res: Response) => controller.update(req, res));
 
-router.delete('/:supplierId', (req, res) => controller.delete(req, res));
+router.delete('/:supplierId', (req: Request, res: Response) => controller.delete(req, res));
 
 export default router;
