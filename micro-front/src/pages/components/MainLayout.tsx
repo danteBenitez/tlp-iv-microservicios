ad6ee0e37dcaeb3bffe45a06c705a54182bd092d
rikrdo pede <ricardopedemonte2018@gmail.com>
2024-10-29 11:33:11 -0300

    fix: implements outlet router-dom

--- micro-front/src/pages/components/MainLayout.tsx
+++ micro-front/src/pages/components/MainLayout.tsx
@@ -1,18 +1,16 @@
-import { Outlet, Route, Routes, useLocation } from 'react-router-dom';
-import { useEffect, useRef, useState } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import NavigationBar from './NavigationBar';
 import Sidebar from './SideBar';
 import Footer from './Footer';
 import FixedPlugin from './FixedPlugin';
 import routes from './routes';
-// import PrivateRoute from '../../routers/PrivateRoutes';
 import defaultImage from '../../assets/img/sidebar-3.jpg';
 
 const MainLayout = () => {
   const [hasImage, setHasImage] = useState(true);
   const [color, setColor] = useState("");
   const [image, setImage] = useState(defaultImage);
-  // const mainPanel = useRef<HTMLDivElement>(null);
   const location = useLocation();
   const [sidebarShow, setSidebarShow] = useState(false);
 
@@ -21,41 +19,10 @@ const MainLayout = () => {
     if (document.scrollingElement) {
       document.scrollingElement.scrollTop = 0;
     }
-    // if (mainPanel.current) {
-    //   mainPanel.current.scrollTop = 0;
-    // }
   }, [location]);
 
   const handleSidebarToggle = () => setSidebarShow(true);
   const handleSidebarClose = () => setSidebarShow(false);
-
-  // interface RouteType {
-  //   isPrivate?: any;
-  //   layout: string;
-  //   path: string;
-  //   component: React.FC<{}> | (() => JSX.Element);
-  // }
-
-  // const getRoutes = (routes: RouteType[]) => {
-  //   return routes.map((prop: RouteType, key) => {
-  //     if (prop.layout === "/admin") {
-  //       return prop.isPrivate ? (
-  //         <Route
-  //           path={prop.path}
-  //           element={<PrivateRoute component={prop.component} />}
-  //           key={key}
-  //         />
-  //       ) : (
-  //         <Route
-  //           path={prop.path}
-  //           element={<prop.component />}
-  //           key={key}
-  //         />
-  //       );
-  //     }
-  //     return null;
-  //   });
-  // };
 
   return (
     <>
@@ -67,15 +34,6 @@ const MainLayout = () => {
         routes={routes}
       />
       <NavigationBar mobileSidebarToggle={handleSidebarToggle} />
-      {/* <div className='mx-4' ref={mainPanel}
-      style={{ overflowY: 'auto', width: 'auto', height: '100vh', minHeight: '50vh' }}
-      >
-        <div className="content">
-          <Routes>
-            {getRoutes(routes)}
-          </Routes>
-        </div>
-      </div> */}
       <div style={{ overflowY: 'auto', width: 'auto', height: '100vh', minHeight: '50vh' }}>
         <Outlet />
       </div>
