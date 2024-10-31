2c8b80e784d63a1565215c9d308314263c3115aa
danteBenitez <benitezdante123@gmail.com>
2024-10-31 11:10:36 -0300

    fix: Manejar estado de carga de autenticación

--- micro-front/src/routers/PrivateRoutes.tsx
+++ micro-front/src/routers/PrivateRoutes.tsx
@@ -1,17 +1,33 @@
-import React, { ComponentType } from 'react';
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { RootState } from './types';
+import React, { ComponentType } from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { RootState } from "./types";
 
 interface PrivateRouteProps {
   component: ComponentType<any>;
   [key: string]: any;
 }
 
-const PrivateRoute: React.FC<PrivateRouteProps> = ({ component: Component, ...rest }) => {
-  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+const PrivateRoute: React.FC<PrivateRouteProps> = ({
+  component: Component,
+  ...rest
+}) => {
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
+  const isLoading = useSelector(
+    (state: RootState) => state.auth.loadingAuthentication
+  );
 
-  return isAuthenticated ? <Component {...rest} /> : <Navigate to="/auth/login" />;
+  if (isLoading) {
+    return null;
+  }
+
+  return isAuthenticated ? (
+    <Component {...rest} />
+  ) : (
+    <Navigate to="/auth/login" />
+  );
 };
 
 export default PrivateRoute;
