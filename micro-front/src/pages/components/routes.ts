51bb02cb8f5afd1b49c42c3900cdaa7e9e3998a4
danteBenitez <benitezdante123@gmail.com>
2024-10-31 11:18:57 -0300

    fix: Poner ruta de proveedores como privada

--- micro-front/src/pages/components/routes.ts
+++ micro-front/src/pages/components/routes.ts
@@ -99,7 +99,7 @@ const routes: TRoute[] = [
     name: "Proveedores",
     component: ProveedoresIndex,
     layout: "/admin",
-    isPrivate: false,
+    isPrivate: true,
     icon: FaUserCircle,
   }
 ];
