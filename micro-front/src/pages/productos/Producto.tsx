0a281595b75bad44baa29537d9faefe03d220f30
danteBenitez <benitezdante123@gmail.com>
2024-10-30 21:14:44 -0300

    chore: Retoques de estilo a Producto

--- micro-front/src/pages/productos/Producto.tsx
+++ micro-front/src/pages/productos/Producto.tsx
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Button, Card, CardImg, Col, Container, Row } from "react-bootstrap";
+import {
+  Badge,
+  Button,
+  Card,
+  CardImg,
+  Col,
+  Container,
+  Row,
+} from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import productImagePlaceholder from "../../assets/img/product-placeholder.jpg";
 import { fetchProductById } from "../../store/services/productService";
@@ -28,30 +36,40 @@ const Producto: React.FC = () => {
   }
 
   return (
-    <Container className="my-5">
+    <Container className="my-5 fs-3">
       {producto.images[0] ? (
         <CardImg
+          style={{ maxHeight: 300 }}
           src={resolveImageUrl(
             producto.images[0]._id ?? productImagePlaceholder
           )}
         ></CardImg>
       ) : (
-        <CardImg src={productImagePlaceholder}></CardImg>
+        <CardImg
+          style={{ maxHeight: 300, objectFit: "cover" }}
+          src={productImagePlaceholder}
+        ></CardImg>
       )}
-      <Row>
+      <Row className="p-4">
         <Col>
-          <h1>Producto: {producto.name}</h1>
+          <h1 className="fs-1 fw-bolder">Producto: {producto.name}</h1>
         </Col>
       </Row>
-      <Row>
+      <Row className="p-2">
         <Col md={8}>
           <Card>
             <Card.Body>
-              <Card.Title className="">{producto.name}</Card.Title>
               <Card.Text>{producto.description}</Card.Text>
               <Card.Text>Marca: {producto.brand}</Card.Text>
-              <Card.Text>Tags: {producto.tags.join(", ")}</Card.Text>
-              <Button variant="primary">Comprar</Button>
+              <Card.Text>
+                Tags:{" "}
+                {producto.tags.map((t) => (
+                  <Badge>{t}</Badge>
+                ))}
+              </Card.Text>
+              <Button variant="primary" size="lg">
+                Comprar
+              </Button>
             </Card.Body>
           </Card>
         </Col>
